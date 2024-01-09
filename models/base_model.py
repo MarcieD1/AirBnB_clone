50c48ffa4d9fd395f7d06371a466f2d49453d2c8
ZaneleNe <zanelenema97@gmail.com>
2024-01-09 12:56:06 -0800

    base model

---
+++ models/base_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""This script is the base model"""
+
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+
+    """Class from which all other classes will inherit"""
+
+    def _init_(self, *args, **kwargs):
+        """Initializes instance attributes
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self._dict_["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self._dict_["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self._dict_[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def _str_(self):
+        """Returns official string representation"""
+
+        return "[{}] ({}) {}".\
+            format(type(self)._name, self.id, self.__dict_)
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of _dict_"""
+
+        my_dict = self._dict_.copy()
+        my_dict["_class"] = type(self).__name_
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
