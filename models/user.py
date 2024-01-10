96a8ef25a8dcfb0519ce1ea3615e8ec9e5c2cafb
ZaneleNe <zanelenema97@gmail.com>
2024-01-10 08:32:42 -0800

    city

--- models/user.py
+++ models/user.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""This module defines the State class"""
+"""This module creates a User class"""
+
 from models.base_model import BaseModel
+
 
 class State(BaseModel):
     """Class for managing state objects"""
 
-    def _init_(self, *args, **kwargs):
-        """Initializes State instance"""
-        super()._init_(*args, **kwargs)
-        self.name = ""
-
-    def _str_(self):
-        """Returns a string representation of the State instance"""
-        return "[State] ({}) {}".format(self.id, self._dict_)
+    name = ""
